40745a8081424496aad206e97fcdd63c742e7150
Sami <78439283+mhmdsami@users.noreply.github.com>
2022-09-12 21:20:20 -0700

    add: addition with indirect addressing mode

---
+++ 8_Bit_Addition_Indirect.asm
@@ -0,0 +1,17 @@
+data segment
+    a db 04
+    b db 02
+    c db ?
+  data ends
+
+code segment
+    assume CS: code, DS: data
+    start:
+    mov ax, data
+    mov ds, ax
+    mov al, a
+    mov bl, b
+    add al, bl ;8-bit subtration
+    mov c, al
+    code ends
+end start
