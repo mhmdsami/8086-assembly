6f3924bdb09dd94cbf16e57294feeadc721103d3
Sami <78439283+mhmdsami@users.noreply.github.com>
2022-09-18 23:02:28 -0700

    add factorial

---
+++ Factorial.asm
@@ -0,0 +1,19 @@
+data segment
+    f db 1
+    n db 5
+    data ends 
+
+code segment
+    assume CS: code, DS: data
+    start:
+    mov ax, data
+    mov ds, ax
+    mov al, 1
+    mov cl, 5
+    fact:
+    mul cl   
+    loop fact
+    mov f, al
+    code ends
+end start
+    
